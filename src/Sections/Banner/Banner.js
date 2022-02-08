f834b1914afb592ef035861eeaf46a60e9a79d2a
kimyoosang <yoo99485@gmail.com>
2022-02-08 17:10:10 +0900

    [Update] Banner animation

--- src/Sections/Banner/Banner.js
+++ src/Sections/Banner/Banner.js
@@ -9,9 +9,9 @@ function Banner() {
         <div className="banner">
           <h1 className="banner-heading">
             <span className="heading-1">Business Ideas</span>
-            {/* <span className="heading-2">Consulting</span>
+            <span className="heading-2">Consulting</span>
             <span className="heading-3">Digital Margeting</span>
-            <span className="heading-4">Strategy</span> */}
+            <span className="heading-4">Strategy</span>
           </h1>
           <p className="banner-paragraph">Lorem ipsum, dolor sit </p>
           <button className="banner-btn">Discover Now</button>
