1cdf1c249a77d2b1e0eee67c461437dd1c267c32
kimyoosang <yoo99485@gmail.com>
2022-02-08 19:10:28 +0900

    [Update] videos conponent style

--- src/Sections/Videos/Videos.js
+++ src/Sections/Videos/Videos.js
@@ -1,31 +1,42 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Video from "../../images/about-us-video.mp4";
 import "./Videos.css";
 
 function Videos() {
+  const videoPlayer = useRef();
+  const [currentTime, setCurrentTime] = useState(0);
+  const [seekValue, setSeekValue] = useState(0);
+
+  const play = () => {
+    videoPlayer.current.play();
+  };
+  const pause = () => {
+    videoPlayer.current.pause();
+  };
   return (
-    <section className="section-2">
-      <h1 className="section-heading">About Us</h1>
-      <div className="section-heading-line"></div>
-      <div className="video-wrapper">
-        <video src={Video} className="video"></video>
-        <div className="controls">
-          <div className="video-bar-wrapper">
-            <div className="video-bar"></div>
+    <section class="section-2" id="about-us">
+      <h1 class="section-heading">About Us</h1>
+      {/* <div class="section-heading-line"></div> */}
+      <div class="video-wrapper">
+        <video src={Video} class="video"></video>
+        <div class="controls">
+          <div class="video-bar-wrapper">
+            <div class="video-bar"></div>
           </div>
-          <div className="buttons">
-            <button>
-              <i className="far fa-play-circle"></i>
+          <div class="buttons">
+            <button id="play-pause">
+              <i class="far fa-play-circle"></i>
             </button>
           </div>
         </div>
       </div>
-      <p className="section-2-paragraph">
-        <i className="fas fa-quote-left"></i>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae quod
-        vero magnam, voluptatem aliquid quisquam consectetur possimus cupiditate
-        maiores et quis nihil? Tempora perferendis neque a incidunt porro quis
-        enim?
+      <p class="section-2-paragraph">
+        <i class="fas fa-quote-left"></i>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+        voluptatibus ipsum expedita perspiciatis unde repellat eligendi quisquam
+        optio neque sint consequuntur, dolore delectus nisi eveniet nostrum
+        explicabo. Quiarepellat, ut. Lorem ipsum dolor sit amet. Lorem ipsum
+        dolor sit amet. Lorem ipsum dolor sit amet.
         <i class="fas fa-quote-right"></i>
       </p>
     </section>
