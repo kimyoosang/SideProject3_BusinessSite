f5f517f91342ea44067fbe7fb76ba0f2ada4ccee
kimyoosang <yoo99485@gmail.com>
2022-02-08 17:18:10 +0900

    [Add] Navbar component

---
+++ src/Sections/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <nav className="navbar">
+        <div className="logo">
+          <a href="#">
+            <span>C</span>ode <span>A</span>nd <span>C</span>reate
+          </a>
+        </div>
+        <div className="nav-list">
+          <a href="#" className="nav-link">
+            Home
+          </a>
+          <a href="#" className="nav-link">
+            About
+          </a>
+          <a href="#" className="nav-link">
+            Pricing
+          </a>
+          <a href="#" className="nav-link">
+            Blog
+          </a>
+          <a href="#" className="nav-link">
+            Contact
+          </a>
+        </div>
+      </nav>
+      <div className="mebu">
+        <div className="line line-1"></div>
+        <div className="line line-2"></div>
+        <div className="line line-3"></div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,6 +1,10 @@
 import Banner from "../Sections/Banner/Banner";
-
+import Navbar from "../Sections/Navbar/Navbar";
 export const TOTAL_SCREENS = [
+  {
+    screen_name: "Navbar",
+    component: Navbar,
+  },
   {
     screen_name: "Banner",
     component: Banner,
