34a3a7ed3495d8d295c47307a0346935a8164687
kimyoosang <yoo99485@gmail.com>
2022-02-08 19:38:43 +0900

    [Update] Videos control

--- src/Sections/Videos/Videos.js
+++ src/Sections/Videos/Videos.js
@@ -1,31 +1,47 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Video from "../../images/about-us-video.mp4";
 import "./Videos.css";
 
 function Videos() {
   const videoPlayer = useRef();
-  const [currentTime, setCurrentTime] = useState(0);
-  const [seekValue, setSeekValue] = useState(0);
+  const videoBar = useRef();
+  const btn = useRef();
 
   const play = () => {
-    videoPlayer.current.play();
+    if (videoPlayer.current.paused) {
+      videoPlayer.current.play();
+      videoPlayer.current.style.opacity = ".7";
+      btn.current.className = "far fa-pause-circle";
+    } else {
+      pause();
+      videoPlayer.current.style.opacity = ".3";
+      btn.current.className = "far fa-play-circle";
+    }
   };
   const pause = () => {
     videoPlayer.current.pause();
   };
+  useEffect(() => {
+    videoPlayer.current.addEventListener("timeupdate", () => {
+      const barWidth =
+        videoPlayer.current.currentTime / videoPlayer.current.duration;
+      videoBar.current.style.width = `${barWidth * 100}%`;
+    });
+  });
+
   return (
     <section class="section-2" id="about-us">
       <h1 class="section-heading">About Us</h1>
       {/* <div class="section-heading-line"></div> */}
       <div class="video-wrapper">
-        <video src={Video} class="video"></video>
+        <video src={Video} class="video" ref={videoPlayer}></video>
         <div class="controls">
           <div class="video-bar-wrapper">
-            <div class="video-bar"></div>
+            <div class="video-bar" ref={videoBar}></div>
           </div>
           <div class="buttons">
-            <button id="play-pause">
-              <i class="far fa-play-circle"></i>
+            <button id="play-pause" onClick={play}>
+              <i class="far fa-play-circle" ref={btn}></i>
             </button>
           </div>
         </div>
