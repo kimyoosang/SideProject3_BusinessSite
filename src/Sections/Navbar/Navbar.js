e775640fa809e4a5e033745e7ce5729d205b06bc
kimyoosang <yoo99485@gmail.com>
2022-02-08 16:25:48 +0900

    [Add] Navbar component

---
+++ src/Sections/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <title>Business Agency</title>
+      <section className="section-1">
+        <div className="banner">
+          <h1 className="banner-heading">
+            <span className="heading-1">Business Ideas</span>
+            <span className="heading-1">Consulting</span>
+            <span className="heading-1">Digital Margeting</span>
+            <span className="heading-1">Strategy</span>
+          </h1>
+          <p className="banner-patagtaph">Lorem ipsum, dolor sit </p>
+          <button className="banner-btn">Discover Now</button>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Navbar;
--- src/utils/commonUtils.js
+++ src/utils/commonUtils.js
@@ -1,6 +1,8 @@
+import Navbar from "../Sections/Navbar/Navbar";
+
 export const TOTAL_SCREENS = [
-  // {
-  //   screen_name: "Navbar",
-  //   component: Navbar,
-  // },
+  {
+    screen_name: "Navbar",
+    component: Navbar,
+  },
 ];
