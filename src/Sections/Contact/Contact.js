d12a48ffbd97f4d453743da2b8e4611eac6fe248
kimyoosang <yoo99485@gmail.com>
2022-02-09 19:20:27 +0900

    [Update] Contact style

--- src/Sections/Contact/Contact.js
+++ src/Sections/Contact/Contact.js
@@ -1,36 +1,37 @@
 import React from "react";
+import "./Contact.css";
 
 function Contact() {
   return (
-    <section className="section-4">
-      <h1 className="section-heading">Contact</h1>
-      <div className="section-heading-line"></div>
-      <div className="contact-wrapper">
-        <div className="contact-details">
-          <div className="phone">
-            <i className="fas fa-mobile-alt"></i>
+    <section class="section-4" id="contact">
+      <h1 class="section-heading">Contact</h1>
+      <div class="section-heading-line"></div>
+      <div class="contact-wrapper">
+        <div class="contact-details">
+          <div class="phone">
+            <i class="fas fa-mobile-alt"></i>
             <h3>Phone</h3>
-            <p>01012341234</p>
-            <p>0212341234</p>
+            <p>010-7470-9948</p>
+            <p>02-1234-1234</p>
           </div>
-          <div className="address">
-            <i className="fas fa-map-marker-alt"></i>
+          <div class="address">
+            <i class="fas fa-map-marker-alt"></i>
             <h3>Address</h3>
-            <p>Main Street</p>
-            <p>Wall Street</p>
+            <p>Main street #123</p>
+            <p>Wall street #123</p>
           </div>
-          <div className="email">
-            <i className="fas fa-envelope"></i>
+          <div class="email">
+            <i class="far fa-envelope"></i>
             <h3>Email</h3>
-            <p>yoo99485@gmail.com</p>
+            <p>yoo99485t@gmail.com</p>
             <p>sales@gmail.com</p>
           </div>
         </div>
         <h1>Get In Touch</h1>
-        <form className="contact-form">
-          <input type="text" placeholder="kimyoosang" />
+        <form class="contact-form">
+          <input type="text" placeholder="Kim yoosang" />
           <input type="email" placeholder="yoo99485@gmail.com" />
-          <textarea placeholder="연락합니다"></textarea>
+          <textarea placeholder="문의합니다"></textarea>
           <input type="submit" value="Send Message" />
         </form>
       </div>
